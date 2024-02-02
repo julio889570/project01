371c2156ccd6418503f2384e8697f932d7d92347
julio889570 <julio889570@gmail.com>
2024-02-01 21:32:35 -0800

    first

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import{ View, Text, SafeAreaView } from 'react-native'
+
+export default function App(){
+  return(
+<SafeAreaView>
+  <View>
+    <Text>Hello World!!</Text>
+  </View>
+</SafeAreaView>
+  )
+
+}
